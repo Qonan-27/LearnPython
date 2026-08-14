7a579ebcceb53435edfd6d34a7051bd07054a6f8
Jerry <jerikristianto4@gmail.com>
2026-08-14 16:12:07 +0700

    Membuat class pertama

--- program.py
+++ program.py
@@ -1,3 +1,12 @@
 # Latihan pertama
 
 #Di file ini saya akan memnuat fungsi
+class Rabbit:
+    def __init__(self,a,b,):
+        self.a = a
+        self.b = b
+
+obj = Rabbit(12,11)
+
+print(obj.a)
+print(obj.b)
